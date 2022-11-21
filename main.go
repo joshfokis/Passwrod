7720ac18fcbb72dfc8e6f62eb80cc5559dbb607d
Josh <joshfokis@gmail.com>
2022-11-20 23:42:38 -0600

    TODO: Add Bubbletea TUI

--- main.go
+++ main.go
@@ -4,85 +4,31 @@ package main
 import (
 	"fmt"
 
-	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	P "passwrod/passwrod"
 
 )
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
-type item struct {
-	title, desc string
-}
-
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.title }
 
 type model struct {
-	list list.Model
+	inputs []textinput.Model
+	focused int
+	err error
 }
 
-
-// VIEW
-
-func (m model) View() string {
-	return docStyle.Render(m.list.View())
-}
-
-// UPDATE
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" {
-			return m, tea.Quit
-		}	
-	case tea.WindowSizeMsg:
-		h, v := docStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
-	}
-
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
-}
-
-// main function for the tui
-func (m model) Init() tea.Cmd {
-	return nil
+fun initialModel() model {
+	var inputs []textinput.Model = make([]textinput.Model, 3)
+	inputs[]
 }
 
 func main() {
-	password := []byte(P.Ask("Please enter your password"))
-	items := []list.Item{}
-
- 	vault := P.Vault{
- 		MasterPassword: password,
- 		File: "vault.json",
- 	}
-
- 	 vault.LoadEntries()
-
-	for _, entry := range vault.Entries {
- 		items = append(items, item{title: entry.Name, desc: entry.Site})
- 	}
-
-	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = "Password Entries"
-
-	p := tea.NewProgram(
-		m, tea.WithAltScreen(),
-	)
-	
-	fmt.Printf("\n%q\n", m.list.Items() )
+	p := tea.NewProgram(initialModel())
 
 	if err := p.Start(); err != nil {
-		fmt.Printf("\n%q\n",err)
-		panic(err)
+		fmt.Println(err)
 	}
 }
-
 // // Package main for passwrod
 // package main
 // 
